72b8fd35f3cfbf7316c7fca601af2ab4bc6b3ef5
Stacy Curry <email": null>
2017-05-05 14:14:33 -0500

    complete MVP

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,7 +6,12 @@ post '/' do
   quote = URI.encode(params[:quote])
   quote_search_url = "http://www.imdb.com/search/text?realm=title&field=quotes&q=#{quote}"
   movie_quote_page_url = MovieQuoteParser.movie_quote_page(quote_search_url)
-  @quotes = MovieQuoteParser.parse_quote_data(movie_quote_page_url)[0..9]
-  @title = MovieQuoteParser.movie_title(quote_search_url)
-  erb :_quote_display, layout: false, locals: { quotes: @quotes }
+  if movie_quote_page_url
+    @quotes = MovieQuoteParser.parse_quote_data(movie_quote_page_url)
+    @title = MovieQuoteParser.movie_title(quote_search_url)
+    erb :_quote_display, layout: false, locals: { quotes: @quotes }
+  else
+    @errors = ["This is the worst! We couldn't find that quote.", "Try searching for another one."]
+    erb :_errors, layout: false
+  end
 end
